5476d1b49b71ad33d7392a0b146b66b7f077c779
WajsiniO <alekswajs@interia.pl>
2023-11-09 16:02:57 +0100

    Strzelnica_update1
    Działa zmiana pozycji

--- Assets/_Scripts/DisappearManager.cs
+++ Assets/_Scripts/DisappearManager.cs
@@ -5,25 +5,16 @@ public class DisappearManager : MonoBehaviour
 {
     public GameObject target;
 
+   
     public void DisappearAndMove()
     {
-        target.SetActive(false); // Ukryj cel
+       
 
-        float randomZ = Random.Range(-9.0f, -2.0f); // Losowa koordynata Z od -2 do -7
-        float randomY = Random.Range(1.0f, 3.0f); 
-        float randomX = Random.Range(17.0f, 20.0f); 
+        float randomZ = Random.Range(-7.0f, -2.0f); // Losowa koordynata Z od -2 do -7
+        float randomY = Random.Range(1.0f, 3.0f); // Losowa koordynata Z od -2 do -7
+        float randomX = Random.Range(17.0f, 20.0f); // Losowa koordynata Z od -2 do -7
         transform.position = new Vector3(randomX, randomY, randomZ);
     }
 
-    IEnumerator DisappearAndAppear()
-    {
-        //while (true)
-       // {
-            DisappearAndMove();
-
-            yield return new WaitForSeconds(2.0f); // Poczekaj kolejne 4 sekundy
-
-            target.SetActive(true); // Pokaż cel
-        //}
-    }
+    
 }
