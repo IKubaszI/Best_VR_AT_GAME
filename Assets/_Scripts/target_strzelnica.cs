1a08d665eb4ee6bdc56ecb92b811c2247a44ee95
IKubaszI <123005268+IKubaszI@users.noreply.github.com>
2023-11-16 14:36:24 +0100

    dodanie zliczania

--- Assets/_Scripts/target_strzelnica.cs
+++ Assets/_Scripts/target_strzelnica.cs
@@ -1,11 +1,17 @@
 using UnityEngine;
+using TMPro;
+
 public class target_strzelnica : MonoBehaviour
 {
     public DisappearManager disappearManager;
+    public TMP_Text scoreText;
+
+    private int score = 0;
 
     void Start()
     {
         disappearManager = transform.parent.GetComponent<DisappearManager>();
+        scoreText.text = "Wynik: " + score.ToString();
     }
 
     void OnCollisionEnter(Collision collision)
@@ -13,7 +19,20 @@ public class target_strzelnica : MonoBehaviour
         if (collision.gameObject.CompareTag("Bullet"))
         {
             Debug.Log("Trafiono w tarczę!");
+            AddPoints(100); // Dodaj 100 pkt za trafienie
             disappearManager.DisappearAndMove();
         }
+    }
+
+    void AddPoints(int points)
+    {
+        score += points;
+        UpdateScoreText();
+
+    }
+
+    void UpdateScoreText()
+    {
+        scoreText.text = "Twoj wynik: " + score.ToString();
     }
 }
