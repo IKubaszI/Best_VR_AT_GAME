b357b4d458fcd096e9344b8b311e4d6518d7776b
IKubaszI <123005268+IKubaszI@users.noreply.github.com>
2023-11-16 15:39:07 +0100

    Naprawa znikania

--- Assets/_Scripts/DisappearManager.cs
+++ Assets/_Scripts/DisappearManager.cs
@@ -7,23 +7,10 @@ public class DisappearManager : MonoBehaviour
 
     public void DisappearAndMove()
     {
-        target.SetActive(false); // Ukryj cel
-
         float randomZ = Random.Range(-9.0f, -2.0f); // Losowa koordynata Z od -2 do -7
         float randomY = Random.Range(1.0f, 3.0f); 
         float randomX = Random.Range(17.0f, 20.0f); 
         transform.position = new Vector3(randomX, randomY, randomZ);
     }
 
-    IEnumerator DisappearAndAppear()
-    {
-        //while (true)
-       // {
-            DisappearAndMove();
-
-            yield return new WaitForSeconds(2.0f); // Poczekaj kolejne 4 sekundy
-
-            target.SetActive(true); // Pokaż cel
-        //}
-    }
 }
