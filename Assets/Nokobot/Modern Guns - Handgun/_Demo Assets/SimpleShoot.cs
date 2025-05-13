68445d1ecf28945baa8dc0c4b0102ca47de69e92
IKubaszI <kubasz2231@gmail.com>
2025-05-13 19:18:23 +0200

    implementacja ilości naboi

--- Assets/Nokobot/Modern Guns - Handgun/_Demo Assets/SimpleShoot.cs
+++ Assets/Nokobot/Modern Guns - Handgun/_Demo Assets/SimpleShoot.cs
@@ -1,123 +1,143 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
+
 [AddComponentMenu("Nokobot/Modern Guns/Simple Shoot")]
 public class SimpleShoot : MonoBehaviour
 {
-    [Header("Prefab Refrences")]
+    [Header("Prefab References")]
     public GameObject bulletPrefab;
     public GameObject casingPrefab;
     public GameObject muzzleFlashPrefab;
 
-    [Header("Location Refrences")]
+    [Header("Location References")]
     [SerializeField] private Animator gunAnimator;
     [SerializeField] private Transform barrelLocation;
     [SerializeField] private Transform casingExitLocation;
 
     [Header("Settings")]
-    [Tooltip("Specify time to destory the casing object")] [SerializeField] private float destroyTimer = 2f;
-    [Tooltip("Bullet Speed")] [SerializeField] private float shotPower = 500f;
-    [Tooltip("Casing Ejection Speed")] [SerializeField] private float ejectPower = 150f;
+    [Tooltip("Czas życia obiektów łusek i flasha")]
+    [SerializeField] private float destroyTimer = 2f;
+    [Tooltip("Siła wystrzału pocisku")]
+    [SerializeField] private float shotPower = 500f;
+    [Tooltip("Siła wyrzutu łuski")]
+    [SerializeField] private float ejectPower = 150f;
 
+    [Header("Audio")]
     public AudioSource source;
     public AudioClip fireSound;
-    public AudioClip reload;
-    public AudioClip noAmmo;
+    public AudioClip reloadSound;
+    public AudioClip noAmmoSound;
+
+    [Header("Magazine & Interaction")]
     public Magazine magazine;
     public XRBaseInteractor socketInteractor;
     private bool hasSlide = true;
 
-
-
-    public void AddMagazine(XRBaseInteractable interactable)
-    {
-        magazine = interactable.GetComponent<Magazine>();
-        source.PlayOneShot(reload);
-        hasSlide=false;
-    }
-    public void RemoveMagazine(XRBaseInteractable interactable)
-    {
-        magazine = null;
-        source.PlayOneShot(reload);
-    }
-    public void Slide()
-    {
-        hasSlide=true;
-        source.PlayOneShot(reload);
-    }
-
-
-
-
-
-
-
     void Start()
     {
+        // Domyślne referencje
         if (barrelLocation == null)
             barrelLocation = transform;
-
         if (gunAnimator == null)
             gunAnimator = GetComponentInChildren<Animator>();
+
+        // Podpinamy eventy do podmieniania magazynka
         socketInteractor.onSelectEntered.AddListener(AddMagazine);
         socketInteractor.onSelectExited.AddListener(RemoveMagazine);
     }
 
     public void PullTheTrigger()
     {
-        if(magazine && magazine.numberOfBullet >0 && hasSlide)
+        // Możemy strzelić tylko gdy jest magazynek, jest w nim >0 pocisków i suwadło jest założone
+        if (magazine != null && magazine.numberOfBullets > 0 && hasSlide)
         {
-        gunAnimator.SetTrigger("Fire");
+            gunAnimator.SetTrigger("Fire");
         }
         else
         {
-            source.PlayOneShot(noAmmo);
+            source.PlayOneShot(noAmmoSound);
         }
     }
 
-
-    //This function creates the bullet behavior
+    // Wywoływane z eventu animacji ("Fire" → poniżej)
     void Shoot()
     {
-        magazine.numberOfBullet--;
+        // Zmniejszamy licznik w magazynku
+        magazine.numberOfBullets--;
+
         source.PlayOneShot(fireSound);
+
+        // Muzzle flash
         if (muzzleFlashPrefab)
         {
-            //Create the muzzle flash
-            GameObject tempFlash;
-            tempFlash = Instantiate(muzzleFlashPrefab, barrelLocation.position, barrelLocation.rotation);
-
-            //Destroy the muzzle flash effect
-            Destroy(tempFlash, destroyTimer);
+            var flash = Instantiate(
+                muzzleFlashPrefab,
+                barrelLocation.position,
+                barrelLocation.rotation
+            );
+            Destroy(flash, destroyTimer);
         }
 
-        //cancels if there's no bullet prefeb
-        if (!bulletPrefab)
-        { return; }
-
-        // Create a bullet and add force on it in direction of the barrel
-        Instantiate(bulletPrefab, barrelLocation.position, barrelLocation.rotation).GetComponent<Rigidbody>().AddForce(barrelLocation.forward * shotPower);
-
+        // Tworzymy pocisk
+        if (bulletPrefab)
+        {
+            var bullet = Instantiate(
+                bulletPrefab,
+                barrelLocation.position,
+                barrelLocation.rotation
+            );
+            bullet.GetComponent<Rigidbody>()
+                  .AddForce(barrelLocation.forward * shotPower);
+        }
     }
 
-    //This function creates a casing at the ejection slot
+    // Wywoływane z eventu animacji ("Fire" → poniżej)
     void CasingRelease()
     {
-        //Cancels function if ejection slot hasn't been set or there's no casing
-        if (!casingExitLocation || !casingPrefab)
-        { return; }
+        if (!casingExitLocation || !casingPrefab) return;
 
-        //Create the casing
-        GameObject tempCasing;
-        tempCasing = Instantiate(casingPrefab, casingExitLocation.position, casingExitLocation.rotation) as GameObject;
-        //Add force on casing to push it out
-        tempCasing.GetComponent<Rigidbody>().AddExplosionForce(Random.Range(ejectPower * 0.7f, ejectPower), (casingExitLocation.position - casingExitLocation.right * 0.3f - casingExitLocation.up * 0.6f), 1f);
-        //Add torque to make casing spin in random direction
-        tempCasing.GetComponent<Rigidbody>().AddTorque(new Vector3(0, Random.Range(100f, 500f), Random.Range(100f, 1000f)), ForceMode.Impulse);
-
-        //Destroy casing after X seconds
-        Destroy(tempCasing, destroyTimer);
+        var casing = Instantiate(
+            casingPrefab,
+            casingExitLocation.position,
+            casingExitLocation.rotation
+        );
+        var rb = casing.GetComponent<Rigidbody>();
+        rb.AddExplosionForce(
+            Random.Range(ejectPower * 0.7f, ejectPower),
+            casingExitLocation.position - casingExitLocation.right * 0.3f - casingExitLocation.up * 0.6f,
+            1f
+        );
+        rb.AddTorque(
+            new Vector3(
+                0,
+                Random.Range(100f, 500f),
+                Random.Range(100f, 1000f)
+            ),
+            ForceMode.Impulse
+        );
+        Destroy(casing, destroyTimer);
     }
 
+    // Wywoływane, gdy wrzucamy nowy magazynek do gniazda
+    public void AddMagazine(XRBaseInteractable interactable)
+    {
+        magazine = interactable.GetComponent<Magazine>();
+        hasSlide = false;
+        source.PlayOneShot(reloadSound);
+    }
+
+    // Wywoływane, gdy wyjmujemy magazynek
+    public void RemoveMagazine(XRBaseInteractable interactable)
+    {
+        magazine = null;
+        source.PlayOneShot(reloadSound);
+    }
+
+    // Wywoływane przy „przeładowaniu” suwadła
+    public void Slide()
+    {
+        hasSlide = true;
+        source.PlayOneShot(reloadSound);
+    }
 }
--- Assets/_Scripts/Magazine.cs
+++ Assets/_Scripts/Magazine.cs
@@ -1,8 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Magazine : MonoBehaviour
 {
-    public int numberOfBullet = 8;
+    [Header("Ilość pocisków w tym magazynku")]
+    public int numberOfBullets = 8;
+
+    [Header("Prefab pustego magazynka")]
+    public GameObject emptyMagazinePrefab;
+
+    // Flaga, żeby jednorazowo podmienić
+    private bool _swapped = false;
+
+    void Update()
+    {
+        // Gdy liczba pocisków spadnie do zera i jeszcze nie podmieniono
+        if (!_swapped && numberOfBullets <= 0)
+            SwapToEmpty();
+    }
+
+    private void SwapToEmpty()
+    {
+        _swapped = true;
+
+        if (emptyMagazinePrefab == null)
+        {
+            Debug.LogWarning($"[{name}] nie przypięto emptyMagazinePrefab!", this);
+            return;
+        }
+
+        // Tworzymy nowy (pusty) magazynek w tej samej pozycji i hierarchii
+        Instantiate(
+            emptyMagazinePrefab,
+            transform.position,
+            transform.rotation,
+            transform.parent
+        );
+
+        // Usuwamy ten (pełny) magazynek
+        Destroy(gameObject);
+    }
 }
