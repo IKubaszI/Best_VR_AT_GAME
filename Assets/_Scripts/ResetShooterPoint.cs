e48d1e8db22f633631ba6374883ae734e97b5f57
IKubaszI <kubasz2231@gmail.com>
2025-05-22 17:20:35 +0200

    poprawy kodu
    Aktulanie działa 50/50

--- Assets/_Scripts/ResetShooterPoint.cs
+++ Assets/_Scripts/ResetShooterPoint.cs
@@ -1,30 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
+// ResetShooterPoint.cs
 using UnityEngine;
 
 public class ResetShooterPoint : MonoBehaviour
 {
-    [SerializeField]
-    private AudioSource audioSource;
-
+    [SerializeField] private AudioSource audioSource;
     public target_strzelnica targetScript; 
     public DisappearManager disappearManager;
-    void OnCollisionEnter(Collision collision)
+
+    /// <summary>
+    /// Wywołaj tę metodę z ButtonVR.OnPress
+    /// </summary>
+    public void ResetOnButton()
+    {
+        Debug.Log("Trafiono w tarczę (przycisk)!");
+        if (targetScript != null)
+        {
+            targetScript.scoreShooter = 0;
+            disappearManager.ResetPosition();
+            targetScript.UpdateScoreText();
+            audioSource.Play();
+        }
+        else
+        {
+            Debug.LogError("Nie znaleziono skryptu target_strzelnica.");
+        }
+    }
+
+    // jeśli nadal chcesz reagować na kule, możesz zostawić:
+    private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Bullet"))
-        {
-            Debug.Log("Trafiono w tarczę!");
-            if (targetScript != null)
-            {
-                targetScript.scoreShooter = 0;
-                disappearManager.ResetPosition();
-                targetScript.UpdateScoreText();
-                audioSource.Play();
-            }
-            else
-            {
-                Debug.LogError("Nie znaleziono skryptu target_strzelnica.");
-            }
-        }
+            ResetOnButton();
     }
 }
